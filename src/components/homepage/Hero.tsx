122d0aca51c0156f7cccfd18b7471b369a90d885
wonj1012 <jjaa1012@gmail.com>
2023-09-05 05:39:40 +0900

    ✨Feat: Add a typing animation for the first page

--- src/components/homepage/Hero.tsx
+++ src/components/homepage/Hero.tsx
@@ -1,15 +1,53 @@
 import clsx from "clsx";
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
+import { TypeAnimation } from "react-type-animation";
 
 import styles from "./Hero.module.scss";
 
 export const Hero: FunctionComponent = () => {
+  const [showAnimation, setShowAnimation] = useState(true);
+  const [subtitleText, setSubtitleText] = useState("👻");
+
+  const toggleAnimation = () => {
+    setShowAnimation(!showAnimation);
+    setSubtitleText(showAnimation ? "👻 Blockchain Developer" : "👻");
+  };
+
   return (
-    <header className={clsx(styles.heroBanner)}>
+    <header
+      className={clsx(styles.heroBanner)}
+      style={{ position: "relative" }}
+    >
+      <div className={clsx(styles.playStopButton)} onClick={toggleAnimation}>
+        {showAnimation ? "⏸" : "▶️"}
+      </div>
       <h1 className={clsx(styles.title)}>
-        <span>Wonjae Choi</span>
+        {showAnimation ? (
+          <TypeAnimation
+            sequence={[
+              "Wonjae Choi",
+              3000,
+              "Blockchain\nDeveloper",
+              1250,
+              "Blockchain\nEngineer",
+              1250,
+              "Blockchain\nResearcher",
+              2000,
+            ]}
+            wrapper="span"
+            speed={30}
+            style={{
+              whiteSpace: "pre-line",
+              fontSize: "1em",
+              display: "inline-block",
+            }}
+            repeat={Infinity}
+          />
+        ) : (
+          <span>Wonjae Choi</span>
+        )}
       </h1>
-      <p className={clsx(styles.subtitle)}>👻 Blockchain Developer</p>
+      <p className={clsx(styles.subtitle)}>{subtitleText}</p>
     </header>
   );
 };
