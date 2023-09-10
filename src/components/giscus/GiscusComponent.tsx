2560f391c03870ac1e3e2206038f938de03d9d8a
wonj1012 <choi@wonj.me>
2023-09-10 15:44:54 +0900

    🎨Style: Add padding-top

--- src/components/giscus/GiscusComponent.tsx
+++ src/components/giscus/GiscusComponent.tsx
@@ -1,26 +1,31 @@
 import React, { FunctionComponent } from "react";
 import Giscus from "@giscus/react";
 import { useColorMode } from "@docusaurus/theme-common";
+import styles from "./GiscusComponent.module.scss"; // SCSS 파일 import
 
-export const GiscusComponent: FunctionComponent = () => {
+const GiscusComponent: FunctionComponent = () => {
   const { colorMode } = useColorMode();
 
   return (
-    <Giscus
-      id="comments"
-      repo="wonj1012/wonj1012.github.io"
-      repoId="R_kgDOKPQ1qg"
-      category="Announcements"
-      categoryId="DIC_kwDOKPQ1qs4CZFmv"
-      mapping="pathname"
-      term="Please leave your comment or question here."
-      strict="0"
-      reactionsEnabled="1"
-      emitMetadata="0"
-      inputPosition="bottom"
-      theme={colorMode}
-      lang="en"
-      loading="lazy"
-    />
+    <div className={styles.giscusWrapper}>
+      <Giscus
+        id="comments"
+        repo="wonj1012/wonj1012.github.io"
+        repoId="R_kgDOKPQ1qg"
+        category="Announcements"
+        categoryId="DIC_kwDOKPQ1qs4CZFmv"
+        mapping="pathname"
+        term="Please leave your comment or question here."
+        strict="0"
+        reactionsEnabled="1"
+        emitMetadata="0"
+        inputPosition="bottom"
+        theme={colorMode}
+        lang="en"
+        loading="lazy"
+      />
+    </div>
   );
 };
+
+export default GiscusComponent;
