c32aa19bbd9aea1682e8dd5cb9633968fdb009fc
Wonjae Choi <choi@wonj.me>
2024-03-26 02:17:21 +0900

    refactor: Refactor markdown page into a component

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,20 +1,5 @@
-import fs from 'fs';
-import Markdown from 'markdown-to-jsx';
-import matter from 'gray-matter';
-import getPostMetadata from '../../../components/getPostMetadata';
-import MainWrapper from '@/components/MainWrapper';
-import Link from 'next/link';
-
-import { Inter, Press_Start_2P, Space_Mono } from 'next/font/google';
-const press = Press_Start_2P({ subsets: ['latin'], weight: ['400'] });
-
-const getPostContent = slug => {
-  const folder = 'posts/';
-  const file = `${folder}${slug}.md`;
-  const content = fs.readFileSync(file, 'utf8');
-  const matterResult = matter(content);
-  return matterResult;
-};
+import getPostMetadata from '@/components/getPostMetadata';
+import MarkdownPage from '@/components/MarkdownPage';
 
 export const generateStaticParams = async () => {
   const posts = getPostMetadata('posts');
@@ -24,85 +9,25 @@ export const generateStaticParams = async () => {
 };
 
 export async function generateMetadata({ params, searchParams }, parent) {
-  // read route params
   const id = params?.slug ? ' ⋅ ' + params?.slug : '';
-
   return {
-    title: `Wonj ⋅ Blog${id.replaceAll('_', ' ')}`,
+    title: `Blog | Wonj${id.replaceAll('_', ' ')}`,
   };
 }
 
 const PostPage = props => {
   const slug = props.params.slug;
-  const post = getPostContent(slug);
-
-  const markdownOptions = {
-    overrides: {
-      h1: {
-        component: ({ children }) => (
-          <h1 className="text-3xl text-pink-500 font-bold">{children}</h1>
-        ),
-      },
-      h2: {
-        component: ({ children }) => (
-          <h2 className="text-2xl text-pink-400 font-bold">{children}</h2>
-        ),
-      },
-      h3: {
-        component: ({ children }) => (
-          <h3 className="text-xl text-pink-300 font-bold">{children}</h3>
-        ),
-      },
-      a: {
-        component: ({ children, href }) => (
-          <a href={href} className="text-pink-500 hover:text-pink-700">
-            {children}
-          </a>
-        ),
-      },
-      strong: {
-        component: ({ children }) => (
-          <strong className="font-bold text-pink-300">{children}</strong>
-        ),
-      },
-      code: {
-        component: ({ children }) => (
-          <code className="bg-gray-800 text-pink-300 px-2 py-1 rounded">
-            {children}
-          </code>
-        ),
-      },
-      blockquote: {
-        component: ({ children }) => (
-          <blockquote className="pl-4 border-l-4 border-gray-400 italic text-gray-500">
-            {children}
-          </blockquote>
-        ),
-      },
-    },
-  };
+  const folder = 'posts';
+  const backLink = '/blog/';
+  const titleColor = 'pink';
 
   return (
-    <MainWrapper>
-      <div className="flex flex-col">
-        <Link
-          href={'/blog'}
-          className="relative mr-auto overflow-hidden group hover:after:translate-x-full after:duration-200 after:absolute after:top-0 after:h-full after:w-full after:right-full after:bg-pink-500 after:z-[-1]"
-        >
-          <p className="group-hover:text-[#030615] duration-200">&larr; Back</p>
-        </Link>
-        <div className="my-12 ">
-          <h1 className={'text-2xl text-pink-100 ' + press.className}>
-            {post.data.title}
-          </h1>
-          <p className="mt-2 text-pink-50">{post.data.date}</p>
-        </div>
-
-        <article className="prose lg:prose-l text-blue-100">
-          <Markdown options={markdownOptions}>{post.content}</Markdown>
-        </article>
-      </div>
-    </MainWrapper>
+    <MarkdownPage
+      folder={folder}
+      slug={slug}
+      backLink={backLink}
+      titleColor={titleColor}
+    />
   );
 };
 
---
+++ components/MarkdownPage.js
@@ -0,0 +1,132 @@
+// components/MarkdownPage.js
+import fs from 'fs';
+import matter from 'gray-matter';
+import Markdown from 'markdown-to-jsx';
+import Link from 'next/link';
+import MainWrapper from '@/components/MainWrapper';
+
+import { Press_Start_2P } from 'next/font/google';
+const press = Press_Start_2P({ subsets: ['latin'], weight: ['400'] });
+
+const titleColorClasses = {
+  50: 'text-purple-50',
+  100: 'text-purple-100',
+  200: 'text-purple-200',
+  300: 'text-purple-300',
+  400: 'text-purple-400',
+  500: 'text-purple-500',
+  'hover-700': 'hover:text-purple-700',
+  'after-bg-500': 'after:bg-purple-500',
+};
+
+const commonOverrides = {
+  blockquote: ({ children }) => (
+    <blockquote className="pl-4 border-l-4 border-gray-400 italic text-gray-500">
+      {children}
+    </blockquote>
+  ),
+};
+
+const getMarkdownContent = (folder, slug) => {
+  const file = `${folder}/${slug}.md`;
+  const content = fs.readFileSync(file, 'utf8');
+  return matter(content);
+};
+
+const MarkdownPage = ({ folder, slug, backLink, titleColor }) => {
+  const { content, data } = getMarkdownContent(folder, slug);
+
+  const markdownOptions = {
+    overrides: {
+      ...commonOverrides,
+      h1: {
+        component: ({ children }) => (
+          <h1 className={`text-3xl ${titleColorClasses[500]} font-bold`}>
+            {children}
+          </h1>
+        ),
+      },
+      h2: {
+        component: ({ children }) => (
+          <h2 className={`text-2xl ${titleColorClasses[400]} font-bold`}>
+            {children}
+          </h2>
+        ),
+      },
+      h3: {
+        component: ({ children }) => (
+          <h3 className={`text-xl ${titleColorClasses[300]} font-bold`}>
+            {children}
+          </h3>
+        ),
+      },
+      a: {
+        component: ({ children, href }) => (
+          <a
+            href={href}
+            className={`${titleColorClasses[500]} ${titleColorClasses['hover-700']}`}
+          >
+            {children}
+          </a>
+        ),
+      },
+      strong: {
+        component: ({ children }) => (
+          <strong className={`font-bold ${titleColorClasses[300]}`}>
+            {children}
+          </strong>
+        ),
+      },
+      code: {
+        component: ({ children }) => (
+          <code
+            className={`bg-gray-800 ${titleColorClasses[300]} px-2 py-1 rounded`}
+          >
+            {children}
+          </code>
+        ),
+      },
+      blockquote: {
+        component: ({ children }) => (
+          <blockquote className="pl-4 border-l-4 border-gray-400 italic text-gray-500">
+            {children}
+          </blockquote>
+        ),
+      },
+    },
+  };
+
+  return (
+    <MainWrapper>
+      <div className="flex flex-col">
+        <Link
+          href={backLink}
+          className={`${titleColorClasses['after-bg-500']} text-blue-100 relative mr-auto overflow-hidden group hover:after:translate-x-full after:duration-200 after:absolute after:top-0 after:h-full after:w-full after:right-full after:z-[-1]`}
+        >
+          <p className="group-hover:text-[#030615] duration-200">&larr; Back</p>
+        </Link>
+        <div className="my-12 ">
+          <h1
+            className={`text-2xl ${titleColorClasses[100]} ` + press.className}
+          >
+            {data.title}
+          </h1>
+          <p className={`mt-2 ${titleColorClasses[50]}`}>{data.date}</p>
+        </div>
+        <article className="prose lg:prose-l text-blue-100">
+          <Markdown options={markdownOptions}>{content}</Markdown>
+        </article>
+        <script
+          src="https://utteranc.es/client.js"
+          repo="wonj1012/comments"
+          issue-term="pathname"
+          theme="dark-blue"
+          crossOrigin="anonymous"
+          async
+        ></script>
+      </div>
+    </MainWrapper>
+  );
+};
+
+export default MarkdownPage;
