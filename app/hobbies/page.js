04a80a572d9d2f9486c8eb21d764d0b21dd1d74f
Wonjae Choi <choi@wonj.me>
2024-03-27 03:09:35 +0900

    feat: Disable academia

---
+++ app/hobbies/page.js
@@ -0,0 +1,16 @@
+import MainWrapper from '@/components/MainWrapper';
+import React from 'react';
+
+import { Inter, Press_Start_2P, Roboto_Mono } from 'next/font/google';
+const space = Roboto_Mono({ subsets: ['latin'], weight: ['400', '700'] });
+// const inter = Inter({ subsets: ["latin"] });
+const press = Press_Start_2P({ subsets: ['latin'], weight: ['400'] });
+
+export const metadata = {
+  title: 'Wonj ⋅ Hobbies',
+  description: '',
+};
+
+export default function ExperiencePage() {
+  return <MainWrapper>Will be updated soon ...</MainWrapper>;
+}
