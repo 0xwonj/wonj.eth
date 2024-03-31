f562fdc908a34b76d03d80e3b2b63bad481457f2
Wonjae Choi <choi@wonj.me>
2024-04-01 01:57:08 +0900

    feat: Add more evm chains

--- config/wagmi.js
+++ config/wagmi.js
@@ -1,6 +1,15 @@
 import { defaultWagmiConfig } from '@web3modal/wagmi/react/config';
 import { cookieStorage, createStorage } from 'wagmi';
-import { mainnet, sepolia } from 'wagmi/chains';
+import {
+  mainnet,
+  arbitrum,
+  polygon,
+  optimism,
+  avalanche,
+  bsc,
+  holesky,
+  sepolia,
+} from 'wagmi/chains';
 
 export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
 
@@ -8,12 +17,22 @@ if (!projectId) throw new Error('Project ID is not defined');
 
 const metadata = {
   name: 'Web3Modal',
-  description: 'Web3Modal Example',
+  description: 'Web3Modal for Wonj',
   url: 'https://wonj.me',
   icons: ['https://avatars.githubusercontent.com/u/37784886'],
 };
 
-const chains = [mainnet, sepolia];
+const chains = [
+  mainnet,
+  arbitrum,
+  polygon,
+  optimism,
+  avalanche,
+  bsc,
+  holesky,
+  sepolia,
+];
+
 export const config = defaultWagmiConfig({
   chains,
   projectId,
