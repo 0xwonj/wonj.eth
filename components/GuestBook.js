c50f6eb51ca7ec95edb38e71abab9654e19aab46
Wonjae Choi <choi@wonj.me>
2024-04-02 17:34:35 +0900

    feat: Change the guestbook

--- components/GuestBook.js
+++ components/GuestBook.js
@@ -7,11 +7,12 @@ export default function GuestBook() {
     <>
       <div className="flex flex-col gap-6">
         <div className="flex flex-row gap-4">
-          <h2 className="textGradient font-press-start text-3xl">Guest Book</h2>
-          <span className="text-4xl">📖</span>
+          <h2 className="textGradientPink font-press-start text-3xl">
+            Guest Book
+          </h2>
         </div>
-        <p className="text-blue-300">
-          <span className="font-bold text-blue-200">
+        <p className="text-pink-100">
+          <span className="font-bold text-pink-300">
             Want to leave a note or just say hi?
           </span>
           <br />
