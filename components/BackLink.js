72c33648a4c31325c30bef68373603f8b0dd73b0
Wonjae Choi <choi@wonj.me>
2024-04-03 04:05:40 +0900

    style: Change backlink button design

--- components/BackLink.js
+++ components/BackLink.js
@@ -11,7 +11,10 @@ export default function BackLink(props) {
       href={backLink}
       className={`${titleColorClasses['after-bg-500']} group relative mr-auto overflow-hidden text-blue-100 after:absolute after:right-full after:top-0 after:z-[-1] after:size-full after:duration-200 hover:after:translate-x-full`}
     >
-      <p className="duration-200 group-hover:text-[#030615]">&larr; Back</p>
+      <p className="font-press-start duration-200 group-hover:text-[#030615]">
+        <i className="fa-solid fa-arrow-left text-xl"></i>
+        <span className="ml-3 text-sm">Back</span>
+      </p>
     </Link>
   );
 }
